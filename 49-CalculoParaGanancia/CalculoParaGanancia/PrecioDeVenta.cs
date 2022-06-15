901dcf4ed1ed8edfb9cee46cc0df17194eba2da2
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:59:04 -0300

    Add files via upload

---
+++ 49-CalculoParaGanancia/CalculoParaGanancia/PrecioDeVenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculoParaGanancia
+{
+    class PrecioDeVenta
+    {
+       public int PorcentajeAgregado (int Proveedor)
+        {
+             return (Proveedor * 30)/100;
+        }
+    }
+}
---
+++ 49-CalculoParaGanancia/CalculoParaGanancia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculoParaGanancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* El dueño de una tienda compra un artículo a un precio determinado. 
+             * Obtener el precio en que lo debe vender para obtener una ganancia del 30%.*/
+
+            PrecioDeVenta PrecioFinal = new PrecioDeVenta();
+
+            Console.WriteLine("Ingrese el precio del articulo adquirido para calcular el precio de venta en estanteria:");
+            int PrecioDeVenta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El precio de venta en estanteria para obtener una ganancia del 30% debe ser:");
+            Console.WriteLine(PrecioDeVenta + PrecioFinal.PorcentajeAgregado(PrecioDeVenta));
+
+            Console.ReadKey();
+        }
+    }
+}
